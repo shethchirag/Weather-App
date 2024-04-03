c21b4317477dc464538301e6f320c28613a9af70
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-04-03 21:00:18 +0530

    create topButton part ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import TopButtons from "./components/TopButtons";
+
 function App() {
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400"></div>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+    </div>
   );
 }
 
